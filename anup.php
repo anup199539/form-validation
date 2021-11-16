421de768902fb8472616d56774df2a01a3a3f2c4
Anup kumar gupta <91132999+anup199539@users.noreply.github.com>
2021-11-17 00:26:41 +0530

    Create anup.php

---
+++ anup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="anup.css">   
+    
+    <title>Document</title>
+
+</head>
+<!--
+<body>
+    
+    <form action="thanks.html" onsubmit=" return validate()">
+        FullName: <input id="uname" type="text" placeholder="fullName"> <br> <br>
+        EmailId: <input id="Email" type="email" placeholder="emailId"> <br> <br>
+        Age: <input id="Age" type="number" placeholder="age" max="27" min="1"> <br> <br>
+        Mobile: <input id="mob" type="text" placeholder="mobile " -mo> <br> <br>
+        DOB: <input id="DOB" type="date" placeholder="date of birth"> <br> <br>
+        <button type="submit">submit</button>
+    </form>                        -->
+
+   <div class="div1">
+          
+
+   </div>
+
+
+</body>
+<script src="anup.js"></script>
+
+
+
+</html>
